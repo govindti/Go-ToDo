bd1245a4697e35b4b52f50d4f38dc5a08c0f0d4d
govindti <108tiwari.g@gmail.com>
2025-08-22 01:34:58 +0530

    done todos API

--- server/main.go
+++ server/main.go
@@ -3,12 +3,36 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"sync"
+
+	"github.com/google/uuid"
+)
+
+var (
+	todos     []Todo
+	todoMutex sync.Mutex
 )
 
 type HeathResponse struct {
 	Status  string `json:"status`
 	Message string `json:"message`
+}
+
+type Todo struct {
+	Id        string `json:"id`
+	Task      string `json:"task`
+	Completed bool   `json:"completed`
+}
+
+type TodobyID struct {
+	Status  string `json:"status`
+	Message string `json:"message`
+}
+
+func genrateRandomId() string {
+	return uuid.New().String()
 }
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
@@ -20,8 +44,53 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(health)
 }
+
+func todosHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Method)
+	switch r.Method {
+	case "GET":
+	case "POST":
+		var newTodo Todo
+		body, err := ioutil.ReadAll(r.Body)
+
+		if err != nil {
+			http.Error(w, "Unable to readfrom req body", http.StatusBadRequest)
+		}
+		fmt.Println(body)
+		err = json.Unmarshal(body, &newTodo)
+
+		if err != nil || newTodo.Task == "" {
+			http.Error(w, "No Input", http.StatusBadRequest)
+			return
+		}
+		newTodo.Id = genrateRandomId()
+		todoMutex.Lock()
+		todos = append(todos, newTodo)
+		todoMutex.Unlock()
+
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(newTodo)
+
+	default:
+		http.Error(w, "Method not Allowed", http.StatusMethodNotAllowed)
+	}
+
+}
+
+func todoByIdHandler(w http.ResponseWriter, r *http.Request) {
+	health := HeathResponse{
+		Status:  "Ok",
+		Message: "Api is Up & Running",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(health)
+}
+
 func main() {
 	http.HandleFunc("/health", healthHandler)
+	http.HandleFunc("/todos", todosHandler)
+	http.HandleFunc("/todos/", todoByIdHandler)
 
 	fmt.Println("App is running on PORT 3000")
 
