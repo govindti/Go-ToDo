9ec15f4315b555d5a3469a404f65de7876678e38
govindti <108tiwari.g@gmail.com>
2025-08-22 01:05:40 +0530

    building health API

--- server/main.go
+++ server/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
+type HeathResponse struct {
+	Status  string `json:"status`
+	Message string `json:"message`
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	health := HeathResponse{
+		Status:  "Ok",
+		Message: "Api is Up & Running",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(health)
+}
 func main() {
-    fmt.Println("Hello, Go!")
+	http.HandleFunc("/health", healthHandler)
+
+	fmt.Println("App is running on PORT 3000")
+
+	err := http.ListenAndServe(":3000", nil)
+
+	if err != nil {
+		fmt.Println("Error Starting the App", err)
+	}
+
 }
